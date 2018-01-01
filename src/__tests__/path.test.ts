c2310e181816b925db26c1a850064fffc94e42a0
Mikael Pajunen <mikael.pajunen@gmail.com>
2018-01-01 15:42:11 +0200

    Property path structure and helpers

---
+++ src/__tests__/path.test.ts
@@ -0,0 +1,45 @@
+import test from 'ava'
+import { accessor } from '../path'
+
+const blob = {
+    baz: {
+        bur: 'bob',
+    },
+    foo: 'bar',
+}
+
+test('accessor returns the original object with empty path', t => {
+    const original = accessor()(blob)
+
+    t.is(original.foo, 'bar')
+})
+
+test('accessor returns a property with string path', t => {
+    const foo = accessor('foo')(blob)
+
+    t.is(foo, 'bar')
+})
+
+test('accessor returns a property with array path', t => {
+    const foo = accessor(['foo'])(blob)
+
+    t.is(foo, 'bar')
+})
+
+test('accessor returns a nested property with array path', t => {
+    const bur = accessor(['baz', 'bur'])(blob)
+
+    t.is(bur, 'bob')
+})
+
+test('accessor returns undefined if path property is not found', t => {
+    const ugh = accessor(['ugh'])(blob)
+
+    t.is(ugh, undefined)
+})
+
+test('accessor returns undefined if nested path property is not found', t => {
+    const bug = accessor(['ugh', 'bug'])(blob)
+
+    t.is(bug, undefined)
+})
---
+++ src/path.ts
@@ -0,0 +1,23 @@
+export type Accessor<T> = (value: T) => any // TODO: Proper types?
+
+export type Path = PathStep | PathStep[]
+
+export type PathStep = string // TODO: Add array support / numeric steps?
+
+export function accessor<T>(path?: Path): Accessor<T> {
+    if (path === undefined) {
+        return identity
+    }
+
+    const pathArray = Array.isArray(path) ? path : [path]
+
+    return (value: T) => pathArray.reduce(getProperty, value)
+}
+
+function getProperty<T, K extends keyof T>(value: T | undefined, name: K): T[K] | undefined {
+    return value && value[name]
+}
+
+function identity<T>(value: T): T {
+    return value
+}
