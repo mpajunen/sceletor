fbc55e45c646205c81582892ffef6b54f3511370
Mikael Pajunen <mikael.pajunen@gmail.com>
2019-06-11 16:19:47 +0300

    Configure build and exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as condition from './condition'
+import * as path from './path'
+import { predicate } from './predicate'
+import { simplify } from './simplify'
+
+export { condition, path, predicate, simplify }
