3c1783659242376ab1b3665115e4777ed2f49fdf
Mikael Pajunen <mikael.pajunen@gmail.com>
2019-10-31 23:54:17 +0200

    Treat undefined same as null
    This allows properly checking for an empty value in includedIn conditions.

--- src/path.ts
+++ src/path.ts
@@ -4,14 +4,14 @@ export type Path = PathStep[]
 
 export type PathStep = number | string
 
-export function accessor<T>(path?: Path | PathStep): Accessor<T> {
-    if (path === undefined) {
-        return identity
-    }
-
+export function accessor<T>(path: Path | PathStep = []): Accessor<T> {
     const pathArray = Array.isArray(path) ? path : [path]
 
-    return (value: T) => (pathArray as any[]).reduce(getProperty, value)
+    return (value: T) => {
+        const result = (pathArray as any[]).reduce(getProperty, value)
+
+        return result === undefined ? null : result
+    }
 }
 
 export function combine(first: Path, ...rest: Path[]): Path {
@@ -20,8 +20,4 @@ export function combine(first: Path, ...rest: Path[]): Path {
 
 function getProperty<T, K extends keyof T>(value: T | undefined, name: K): T[K] | undefined {
     return value && value[name]
-}
-
-function identity<T>(value: T): T {
-    return value
 }
